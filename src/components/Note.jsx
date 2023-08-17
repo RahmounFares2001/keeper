f2484a9ad7dc3d33db8809199acb5f18b0267db3
RahmounFares2001 <140554206+RahmounFares2001@users.noreply.github.com>
2023-08-17 21:03:10 +0200

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React , {useState} from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 
 function Note(props) {
-  
+
+  const [id, setId] = useState(props.id);
+  const [title, setTitle] = useState(props.title);
+  const [content, setContent] = useState(props.content);
+
   function handleDelete() {
-    props.onDelete(props.id);
+    props.onDelete(id);
   }
 
   function handleEdit() {
-    props.onEdit(props.id);
+    props.onEdit(id, title, content);
   }
 
   return (
     <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
       <button className="note-del-btn" onClick={handleDelete}>
         <DeleteIcon className="note-icon" />
       </button>
-      <button className="note-ed-btn"  onClick={handleEdit}>
+      <button className="note-ed-btn" onClick={handleEdit}>
         <EditIcon className="note-icon" />
       </button>
     </div>
